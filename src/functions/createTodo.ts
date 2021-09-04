42b6dca8665ba031f6d6036ff72e92c693bc6505
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-09-04 17:36:30 -0300

    feat: add createTodo function

---
+++ src/functions/createTodo.ts
@@ -0,0 +1,36 @@
+import { document } from "src/utils/dynamodbClient";
+import {v4} from "uuid"
+
+interface IEventBody {
+  title: string;
+  deadline: string;
+}
+
+interface IEventPathParameters {
+  user_id: string;
+}
+
+export const handle = async (event) => {
+  const { title, deadline } = JSON.parse(event.body) as IEventBody;
+  const { user_id } = event.pathParameters as IEventPathParameters;
+
+  await document.put({
+    TableName: "todoList",
+    Item: {
+      id: v4(),
+      title,
+      user_id,
+      deadline: new Date(deadline).toISOString()
+    }
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Todo successfully added"
+    }),
+    headers: {
+      "Content-type": "application/json"
+    }
+  }
+}
--- src/functions/hello.ts
+++
@@ -1,9 +0,0 @@
-export const handle = async (event)=>({
-  statusCode:200,
-  body: JSON.stringify({
-    message: "Hello world"
-  }),
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
